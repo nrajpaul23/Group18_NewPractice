13f9c2f1b5653fae9d7430fad8bc2dacf25637fc
KKUSA <kryvianok2012@gmail.com>
2022-01-24 16:53:00 -0500

    print statement 1 commit

--- src/us100/Kseniia/TestCase006.java
+++ src/us100/Kseniia/TestCase006.java
@@ -3,5 +3,7 @@ package us100.Kseniia;
 public class TestCase006 {
     public static void main(String[] args) {
         System.out.println("It's not as easy as it might seem");
+        System.out.println("Let's have a successful pull");
+
     }
 }
